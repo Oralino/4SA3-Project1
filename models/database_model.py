bb7fffca7729b9012ffc731229c074d651842bf8
Noor Alnajar <nooralnajar1997@outlook.com>
2026-04-05 16:15:58 -0400

    Nitpicking

--- models/database_model.py
+++ models/database_model.py
@@ -24,6 +24,7 @@ class ProfileModel:
         client = self.pool.acquire()
         self.collection = client.get_database("engine_optimizer_db").get_collection("game_profiles")
         self.pool.release(client)
+        
     def insert_profile(self, data):
         # Inserts a new document into the MongoDB collection
         self.collection.insert_one(data)
