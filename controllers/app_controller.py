82bc16738a7fcb982a07523cb5236c126ccec551
Noor Alnajar <nooralnajar1997@outlook.com>
2026-04-05 17:30:21 -0400

    Create the app controller class

---
+++ controllers/app_controller.py
@@ -0,0 +1,31 @@
+from models.database_model import ProfileModel
+from models.maps_api import MapsAPIClient
+from strategies.export_strategy import JsonExportStrategy
+
+class AppController:
+    def __init__(self, db_pool, api_key):
+        # Initializes the model layers with the database pool and API key
+        self.model = ProfileModel(db_pool)
+        self.api = MapsAPIClient(api_key)
+        self.export_strategy = JsonExportStrategy()
+
+    def save_profile(self, game, gpu, ini_data):
+        # Formats the data and passes it to the database model
+        data = {
+            "game": game,
+            "gpu": gpu,
+            "ini_data": ini_data
+        }
+        self.model.insert_profile(data)
+
+    def load_profiles(self):
+        # Fetches profiles from the database
+        return self.model.get_all_profiles()
+
+    def delete_profile(self, game_name):
+        # Instructs the database model to remove a specific profile
+        self.model.delete_profile(game_name)
+
+    def fetch_shops(self, query):
+        # Requests local shop data from the API model
+        return self.api.search_shops(query)
