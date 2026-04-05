3b77a959cd248bb08d64224250b2236989d55465
Noor Alnajar <nooralnajar1997@outlook.com>
2026-04-05 17:31:40 -0400

    Basic GUI window added

---
+++ views/main_window.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+class EngineOptimizerView:
+    def __init__(self, root, controller):
+        # Sets up the main application window size and title
+        self.root = root
+        self.controller = controller
+        self.root.title("Engine Optimizer")
+        self.root.geometry("600x500")
